4235e29cbfe2122af382c5e2897c0e3952119102
Mason021 <mdc5054@gmail.com>
2021-08-29 13:12:43 -0500

    added my model and corresponding api routes, formatted existing work

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ require('dotenv').config();
 
 // create the connection to the db, both local or heroku via jawsdb
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
 
 module.exports = sequelize;
